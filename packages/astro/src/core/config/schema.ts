2f6100956f3d770700149d19159e91b9a01674a2
Nate Moore <natemoo-re@users.noreply.github.com>
2023-10-31 15:28:27 -0500

    Add `experimental.contentCollectionCache` config option (#8962)

--- packages/astro/src/core/config/schema.ts
+++ packages/astro/src/core/config/schema.ts
@@ -61,6 +61,7 @@ const ASTRO_CONFIG_DEFAULTS = {
 	experimental: {
 		optimizeHoistedScript: false,
 		devOverlay: false,
+		contentCollectionCache: false,
 	},
 } satisfies AstroUserConfig & { server: { open: boolean } };
 
@@ -323,6 +324,7 @@ export const AstroConfigSchema = z.object({
 				.optional()
 				.default(ASTRO_CONFIG_DEFAULTS.experimental.optimizeHoistedScript),
 			devOverlay: z.boolean().optional().default(ASTRO_CONFIG_DEFAULTS.experimental.devOverlay),
+			contentCollectionCache: z.boolean().optional().default(ASTRO_CONFIG_DEFAULTS.experimental.contentCollectionCache),
 		})
 		.strict(
 			`Invalid or outdated experimental feature.\nCheck for incorrect spelling or outdated Astro version.\nSee https://docs.astro.build/en/reference/configuration-reference/#experimental-flags for a list of all current experiments.`
